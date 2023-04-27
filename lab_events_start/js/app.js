5cc7c43323125b29eec725a2b90e89a2778c8302
Cooper-Higgins <jamie@supplantree.com>
2023-04-27 16:33:08 +0100

    Working with adding new items to list

--- lab_events_start/js/app.js
+++ lab_events_start/js/app.js
@@ -1,3 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
+
+const form = document.querySelector("#new-item-form");
+
+const handleFormSubmit = function(event){
+  event.preventDefault();
+  const newListItem = document.createElement("li")
+  newListItem.textContent = 
+  `Title:
+  ${event.target.title.value}
+  Author:
+  ${event.target.author.value}
+  Category:
+  ${event.target.category.value}
+  `;
+
+  document.querySelector("ul").appendChild(newListItem);
+  document.getElementById("new-item-form").reset();
+}
+form.addEventListener("submit", handleFormSubmit);
+
 })
